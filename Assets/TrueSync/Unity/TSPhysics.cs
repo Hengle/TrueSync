5fa25d92b80ae391b7897fbf0b3ec673f23e5956
chiachenghuang <suzuke@hotmail.com>
2018-06-21 10:56:38 +0800

    TSRayCast never new a ray each time

--- Assets/TrueSync/Unity/TSPhysics.cs
+++ Assets/TrueSync/Unity/TSPhysics.cs
@@ -8,9 +8,11 @@ namespace TrueSync
     **/
     public class TSPhysics {
 
+        private static TSRay ray = new TSRay(TSVector.zero, TSVector.zero);
         public static bool Raycast(TSVector rayOrigin, TSVector rayDirection, out TSRaycastHit hit, FP maxDistance, int layerMask = UnityEngine.Physics.DefaultRaycastLayers)
         {
-            TSRay ray = new TSRay(rayOrigin, direction:rayDirection);
+            ray.origin = rayOrigin;
+            ray.direction = rayDirection;
             hit = PhysicsWorldManager.instance.Raycast(ray, maxDistance, layerMask:layerMask);
             if (hit != null)
             {
