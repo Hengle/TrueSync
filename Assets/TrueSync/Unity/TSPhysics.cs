cffd08db7b022c0b329dd093ba5153e0e7848634
chiachenghuang <suzuke@hotmail.com>
2018-06-29 11:49:29 +0800

    Return directly if rayDirection is zero

--- Assets/TrueSync/Unity/TSPhysics.cs
+++ Assets/TrueSync/Unity/TSPhysics.cs
@@ -11,8 +11,13 @@ namespace TrueSync
         private static TSRay ray = new TSRay(TSVector.zero, TSVector.zero);
         public static bool Raycast(TSVector rayOrigin, TSVector rayDirection, out TSRaycastHit hit, FP maxDistance, int layerMask = UnityEngine.Physics.DefaultRaycastLayers)
         {
+            hit = null;
+            if (rayDirection.x == FP.Zero && rayDirection.y == FP.Zero && rayDirection.z == FP.Zero)
+                return false;
+
             ray.origin = rayOrigin;
             ray.direction = rayDirection;
+
             hit = PhysicsWorldManager.instance.Raycast(ray, maxDistance, layerMask:layerMask);
             if (hit != null)
             {
