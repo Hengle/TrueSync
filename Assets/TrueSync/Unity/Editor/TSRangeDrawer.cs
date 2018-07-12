7888ca1e88c6f5ef50c778868037aa8207e66aa8
chiachenghuang <suzuke@hotmail.com>
2018-07-12 17:52:00 +0800

    Add TSRangeAttribute

---
+++ Assets/TrueSync/Unity/Editor/TSRangeDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace TrueSync
+{
+    [CustomPropertyDrawer(typeof(TSRangeAttribute))]
+    internal sealed class TSRangeDrawer : PropertyDrawer
+    {
+        private FP fpValue;
+
+        //
+        // Methods
+        //
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var rangeAttribute = (TSRangeAttribute)base.attribute;
+
+            var rawProp = property.FindPropertyRelative("_serializedValue");
+
+            fpValue = EditorGUI.Slider(position, label, fpValue.AsFloat(), rangeAttribute.min, rangeAttribute.max);
+
+            fpValue = (fpValue / rangeAttribute.step) * rangeAttribute.step;
+
+            rawProp.longValue = fpValue.RawValue;
+        }
+    }
+}
---
+++ Assets/TrueSync/Unity/TSRangeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace TrueSync
+{
+    public class TSRangeAttribute : PropertyAttribute
+    {
+        public readonly float min;
+        public readonly float max;
+        public readonly float step;
+
+        public TSRangeAttribute(float min, float max, float step = 0.1f)
+        {
+            this.min = min;
+            this.max = max;
+            this.step = step;
+        }
+    }
+}
